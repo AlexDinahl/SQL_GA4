dd87355537fb3f2ac235f0ccc4b49f48c5ec941c
Alex Dinahl <42444452+AlexDinahl@users.noreply.github.com>
2024-04-19 14:48:59 +0200

    Update ga4_information_schema_tables.sql

--- ga4_information_schema_tables.sql
+++ ga4_information_schema_tables.sql
@@ -1,17 +1,31 @@
-select replace(schema_name,'analytics_','') as schema_name, if(regexp_contains(schema_name,'^analytics_'),replace(key,'ga_','ga4_'),key) as label_key
-, value as label_value
-from
-(
- select schema_name, option_name, option_value,
-  array
-  (
-   select as struct arr[offset(0)] key , arr[offset(1)] value 
-   from unnest(regexp_extract_all(option_value, r'STRUCT\(("[^"]+", "[^"]+")\)')) kv, 
-    unnest([struct(split(replace(kv, '"', ''), ', ') as arr)])
-  ) as labels
- from `isg-dwh-bigquery`.`region-EU`.INFORMATION_SCHEMA.SCHEMATA_OPTIONS
- where option_name in ('labels')
-),
-unnest(labels)
-where key='ga_analytics_view'
-order by 2, 3;
+--GA4 and UA tables
+
+SELECT
+  REPLACE(schema_name,'analytics_','') AS schema_name,
+IF
+  (REGEXP_CONTAINS(schema_name,'^analytics_'),REPLACE(KEY,'ga_','ga4_'),KEY) AS label_key,
+  value AS label_value
+FROM (
+  SELECT
+    schema_name,
+    option_name,
+    option_value,
+    ARRAY (
+    SELECT
+      AS STRUCT arr[
+    OFFSET
+      (0)] KEY, arr[
+    OFFSET
+      (1)] value
+    FROM
+      UNNEST(REGEXP_EXTRACT_ALL(option_value, r'STRUCT\(("[^"]+", "[^"]+")\)')) kv, UNNEST([STRUCT(SPLIT(REPLACE(kv, '"', ''), ', ') AS arr)]) ) AS labels
+  FROM
+    `isg-dwh-bigquery`.`region-EU`.INFORMATION_SCHEMA.SCHEMATA_OPTIONS
+  WHERE
+    option_name IN ('labels') ),
+  UNNEST(labels)
+WHERE
+  KEY='ga_analytics_view'
+ORDER BY
+  2,
+  3;
