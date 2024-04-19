9c8f2d709e771280f9f6235af893df6bd71268b6
Alex Dinahl <42444452+AlexDinahl@users.noreply.github.com>
2024-04-19 15:12:04 +0200

    Update ga4_procedural_example.sql

--- ga4_procedural_example.sql
+++ ga4_procedural_example.sql
@@ -11,7 +11,7 @@ END FOR;
 
 create temp table client_id as (
   select replace(schema_name,'analytics_','') as property_id,replace(replace(option_value,'GA4',''),'"','') as option_value
-from `isg-dwh-bigquery`.`region-EU`.INFORMATION_SCHEMA.SCHEMATA_OPTIONS
+from `bigquery`.`region-EU`.INFORMATION_SCHEMA.SCHEMATA_OPTIONS
 where regexp_contains(schema_name,'^analytics_')
 and option_name='description');
 
@@ -22,7 +22,7 @@ select property_id
 from client_id limit 2
 )
 DO
-  execute immediate concat("insert into hostnames ","select distinct device.web_info.hostname from"," `isg-dwh-bigquery.analytics_",item.property_id,".events_20231005`");
+  execute immediate concat("insert into hostnames ","select distinct device.web_info.hostname from"," `bigquery.analytics_",item.property_id,".events_123456789`");
 END FOR;
 
 select * from hostnames;
@@ -33,9 +33,9 @@ select * from hostnames;
 DECLARE r ARRAY <STRING>;
 SET r = []; 
 
-FOR var IN (SELECT column_name FROM `isg-dwh-bigquery`.`analytics_292798251`.INFORMATION_SCHEMA.COLUMNS
+FOR var IN (SELECT column_name FROM `bigquery`.`analytics_123456789`.INFORMATION_SCHEMA.COLUMNS
   WHERE table_name LIKE "events_20240101") DO
-    IF EXISTS(SELECT var.column_name FROM `isg-dwh-bigquery.analytics_292798251.events_20240101`
+    IF EXISTS(SELECT var.column_name FROM `bigquery.analytics_123456789.events_20240101`
       WHERE var.column_name IS NOT NULL) THEN 
         SET r = ARRAY_CONCAT(r, [var.column_name]);
     END IF;
@@ -47,7 +47,7 @@ SELECT r;
 
 
 --/*
-FOR var IN (SELECT distinct column_name FROM isg-dwh-bigquery.analytics_292798251.INFORMATION_SCHEMA.COLUMNS
+FOR var IN (SELECT distinct column_name FROM bigquery.analytics_123456789.INFORMATION_SCHEMA.COLUMNS
   WHERE table_name LIKE "events_20240101" limit 10) 
   DO
   SELECT var.column_name;    
